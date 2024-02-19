68e8bef459fcfd6f0e6fc0dcbb07e21aa0fc98b2
SilasWTurner <silaswturner@icloud.com>
2024-02-19 13:27:50 +0000

    created tasks app

--- under_my_scrum_brella/under_my_scrum_brella/settings.py
+++ under_my_scrum_brella/under_my_scrum_brella/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
-    'users'
+    'users',
+    'tasks',
 ]
 
 MIDDLEWARE = [
