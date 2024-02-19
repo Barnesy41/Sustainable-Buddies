d54161f686bc8637fdc4d23d0d2618778a6db9ed
SilasWTurner <silaswturner@icloud.com>
2024-02-19 13:19:28 +0000

    updated setting

--- under_my_scrum_brella/under_my_scrum_brella/settings.py
+++ under_my_scrum_brella/under_my_scrum_brella/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'under_my_scrum_brella/static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
