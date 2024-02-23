b625ae0b7139a19c340d6089ef051a21c35593be
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2024-02-23 01:40:18 +0000

    bugfix - incorrect URL

--- under_my_scrum_brella/tasks/urls.py
+++ under_my_scrum_brella/tasks/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # ex: /tasks/5/
     path("<int:task_id>/", views.detail, name="detail"),
-    path('tasks/', views.task_list, name='task_list'),
+    path('', views.task_list, name='task_list'),
 ]
