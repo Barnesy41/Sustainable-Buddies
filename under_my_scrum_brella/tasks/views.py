1e236570bb3419bf5121213aa0e9894550337b07
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2024-02-23 15:22:39 +0000

    removed redundant code

--- under_my_scrum_brella/tasks/views.py
+++ under_my_scrum_brella/tasks/views.py
@@ -1,11 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Task
-
-#this is just for testing purposes, can remove later
-#TODO: More thorough error catching
-def detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, "detail.html", {"task": task})
     
 def task_list(request):
     tasks = Task.objects.all()
