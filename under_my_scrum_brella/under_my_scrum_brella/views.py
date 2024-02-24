88e00fa73ed10b199a5186a9817e6515e5eea784
O-Fitzgerald33 <124785657+O-Fitzgerald33@users.noreply.github.com>
2024-02-24 16:08:43 +0000

    Delete views.py

--- under_my_scrum_brella/under_my_scrum_brella/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from users.models import Item
-
-def wardrobe(request):
-    items = Item.objects.all()
-    context = {'items': items}
-    return render(request, 'wardrobe.html', context)
