4f6da85c5d3db39cfc8b7e06bce516e8c05dadcd
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2024-02-26 21:55:30 +0000

    Added contributor information & code clean-up

--- under_my_scrum_brella/tasks/admin.py
+++ under_my_scrum_brella/tasks/admin.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
+###########################################################################
+#   Author: Silas Turner
+#   Contributors: Ollie Barnes
+#
+#   The author has written all code in this file unless stated otherwise.
+###########################################################################
 
+from django.contrib import admin
 from .models import Task, UserTask
 
 admin.site.register(Task)
-admin.site.register(UserTask)
+admin.site.register(UserTask)   #Written by Ollie Barnes
--- under_my_scrum_brella/tasks/views.py
+++ under_my_scrum_brella/tasks/views.py
@@ -1,3 +1,10 @@
+###########################################################################
+#   Author: Ollie Barnes
+#   Contributors: Luke Clarke, Silas Turner
+#
+#   The author has written all code in this file unless stated otherwise.
+###########################################################################
+ 
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Task, UserTask
 from users.models import UserDetail
@@ -6,19 +13,19 @@ from django.contrib import messages
 
 # This is a list of all tasks, NOT a specific user's tasks  #
 # if you want a user's tasks, please use user_tasks instead #
-def task_list(request):
+def task_list(request):     #Written by Luke Clarke
     current_user = request.user
     if not current_user.is_authenticated:
         messages.success(request, "Please login first")
         return redirect('login')
-    if current_user.is_superuser:
-        return redirect('/admin/')
+    if current_user.is_superuser:   #Written by Silas Turner
+        return redirect('/admin/')  #Written by Silas Turner
     if request.method == 'POST':
         task_id = request.POST["task_id"]
         
-        #Update the completion status of the task
+        #Update the completion status of the user's recently completed task from 0 (incomplete) to 1 (complete)
         task_object = UserTask.objects.get(task_id=task_id, user_id=current_user)
-        task_object.completion_status = 1     #update the completion status from 0 (incomplete) to 1 (complete)
+        task_object.completion_status = 1
         task_object.save()
         
         #Add coins & XP to the user's account
@@ -31,17 +38,17 @@ def task_list(request):
         return redirect('tasks')
     
     #Get the list of all tasks
-    #TODO: currently redundant
+    #TODO: now redundant
+    # Written by Luke Clarke
     tasks = Task.objects.all()
     context = {'tasks': tasks}
+    #Written by Luke Clarke End
     
     #Get all tasks assigned to the given user
     assigned_tasks = UserTask.objects.filter(user_id=current_user)
     context['assigned_tasks'] = assigned_tasks
     
-    if current_user.is_authenticated:
-        user_details = get_object_or_404(UserDetail, pk=current_user.id)
-        context['user_details'] = user_details
-        return render(request, 'tasks.html', context)
-    else:
-        return render(request, 'tasks.html', context)
+    #Get the details for the current user
+    user_details = get_object_or_404(UserDetail, pk=current_user.id)
+    context['user_details'] = user_details
+    return render(request, 'tasks.html', context)
