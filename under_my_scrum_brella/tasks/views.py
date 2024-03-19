02df3f286c33cf2d11e4e60d7483dd0b03ec7d5c
JackTheHoooman <jackethanbundy@gmail.com>
2024-03-19 10:17:49 +0000

    Integrated into task page

--- under_my_scrum_brella/tasks/views.py
+++ under_my_scrum_brella/tasks/views.py
@@ -15,14 +15,41 @@ from django.contrib import messages
 from math import sqrt
 
 
+
+# Global for how much happiness increases upon task completion
+taskHappiness = 0.1
+
+
+
+
+######
+# Function to complete tasks. Usese a global var taskHappiness for managing happiness
+######
+def complete_task(user_id, task_id):
+    #Update the completion status of the user's recently completed task from 0 (incomplete) to 1 (complete)
+    task_object = UserTask.objects.get(task_id=task_id, user_id=user_id)
+    task_object.completion_status = 1
+    task_object.save()
+        
+    #Add coins, XP and happiness to the user's account
+    user = UserDetail.objects.get(user=user_id)    
+    task_object = Task.objects.get(id=task_id)
+
+    user.total_coins = user.total_coins + task_object.CoinReward
+    user.total_xp = user.total_xp + task_object.XpReward
+
+    #Luke Clarke - Sets buddy happiness (between 0 and 1)
+    newHappiness = max(0, min(1, user.buddy_happiness + taskHappiness))
+    user.buddy_happiness = newHappiness
+
+    user.save()
+
+
 ###########################################################################
 #   This function is used to render the tasks.html page, 
 #   and provide tasks.html with the required information from the database
 ###########################################################################
 def task_list(request):
-    #How much happiness increases upon task completion
-    taskHappiness = 0.1
-
     #Redirect the user if not authenticated
     current_user = request.user
     if not current_user.is_authenticated:
@@ -36,26 +63,12 @@ def task_list(request):
     #If authenticated, render the tasks.html web page with the required data
     if request.method == 'POST':
         task_id = request.POST["task_id"]
-        
-        #Update the completion status of the user's recently completed task from 0 (incomplete) to 1 (complete)
-        task_object = UserTask.objects.get(task_id=task_id, user_id=current_user)
-        task_object.completion_status = 1
-        task_object.save()
-        
-        #Add coins, XP and happiness to the user's account
-        user = UserDetail.objects.get(user=current_user)    
-        task_object = Task.objects.get(id=task_id)
-
-        user.total_coins = user.total_coins + task_object.CoinReward
-        user.total_xp = user.total_xp + task_object.XpReward
-
-        #Luke Clarke - Sets buddy happiness (between 0 and 1)
-        newHappiness = max(0, min(1, user.buddy_happiness + taskHappiness))
-        user.buddy_happiness = newHappiness
-
-        user.save()
-        
-        return redirect('tasks')
+        if Task.objects.get(id=task_id).QrData: # QrData is not empty
+            print("in here")
+            return redirect('scan')
+        else: # A regular task
+            complete_task(current_user, task_id)
+            return redirect('tasks')
     
     # Written by Luke Clarke
     #Get the list of all tasks
@@ -80,7 +93,7 @@ def scan(request):
         return redirect('login')
 
     if request.method == "POST": 
-        print(request.POST)
+
         task_hash = request.POST["QR-val"]
         task_lat = float(request.POST["Geo-lat"])
         task_long = float(request.POST["Geo-long"])
@@ -89,13 +102,17 @@ def scan(request):
         task_object = Task.objects.get(QrData=task_hash)
         
         if sqrt((task_object.GeoLat-task_lat)**2 + (task_object.GeoLong-task_long)**2) <= task_object.GeoRange:
-            print("Inside range")
-            print(sqrt((task_object.GeoLat-task_lat)**2 + (task_object.GeoLong-task_long)**2))
+            try:
+                complete_task(request.user.id, task_object.pk)
+                return redirect('tasks')
+            except ObjectDoenNotExist:
+                messages.success(request, "You have not been given this task to complete!")
         else:
             messages.success(request, "You are not close enough to the task to complete it!")
 
         return redirect('tasks')
 
 
-    context = {}
+    user_details = get_object_or_404(UserDetail, pk=request.user.id)
+    context = {'user_details' : user_details}
     return render(request, 'scan.html', context)
