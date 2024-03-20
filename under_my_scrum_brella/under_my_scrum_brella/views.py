d5569bd0701398b3f462cb5abea6f28e9603984d
lukebclarke <luke616logins@gmail.com>
2024-03-20 13:24:20 +0000

    Fixed happiness not decaying bug

--- under_my_scrum_brella/under_my_scrum_brella/views.py
+++ under_my_scrum_brella/under_my_scrum_brella/views.py
@@ -8,9 +8,11 @@
 
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.utils import timezone
 
 from users.models import UserDetail
 from items.models import UserItem
+
 
 # Global for how much happiness increases upon task completion
 gameHappiness = 0.05
@@ -44,6 +46,7 @@ def my_pet(request):
         # Oliver Fitzgerald
         worn_user_items = UserItem.objects.filter(user=user, is_worn=True)
         index_array = [user_item.item.item_index for user_item in worn_user_items]
+        decayHappiness(request)
         # gives all of the worn items to the mypet
         context = {
             'user_details': user_details,
@@ -115,3 +118,50 @@ def completeGame(user_id):
     newHappiness = max(0, min(1, user.buddy_happiness + gameHappiness))
     user.buddy_happiness = newHappiness
     user.save()
+
+
+#Luke Clarke - used to update happiness and ensure it does not exceed 1
+def updateHappiness(user, happinessToAdd):
+    if user.is_authenticated:
+        user_details = get_object_or_404(UserDetail, pk=user.id)
+        newHappiness = user_details.buddy_happiness + happinessToAdd
+
+        if newHappiness >= 1:
+            user_details.buddy_happiness = 1
+        elif newHappiness <= 0:
+            user_details.buddy_happiness = 0
+        else:
+            user_details.buddy_happiness = newHappiness
+
+        print("happiness here: " + str(user_details.buddy_happiness))
+        
+        return user_details
+
+#Luke Clarke - checks whether happiness should decay
+def decayHappiness(request):
+    secondsToDecay = 300 #how many seconds until happiness decays
+    decayPerIncrement = 0.01 #how much happiness decays by
+
+    currentUser = request.user
+
+    if currentUser.is_authenticated:
+        user_details = get_object_or_404(UserDetail, pk=currentUser.id)
+        
+        current_time = timezone.now()
+        time_elapsed = (current_time - user_details.last_happiness_decay_time).total_seconds()
+
+        print("user authenticated")
+
+        #decays happiness after set time has passed
+        if (time_elapsed > secondsToDecay):
+            time_segments_elapsed = int(time_elapsed//secondsToDecay)
+            decayValue = time_segments_elapsed * decayPerIncrement
+
+            print(time_elapsed)
+            print(time_segments_elapsed)
+            print("decay by: " + str(decayValue))
+            user_details = updateHappiness(currentUser, -decayValue)
+            user_details.last_happiness_decay_time = current_time
+            print("happiness: " + str(user_details.buddy_happiness))
+            user_details.save()
+            
